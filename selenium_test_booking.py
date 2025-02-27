f2cb5880fe8c977ef0e11f3de28ccf8a01f68417
tekstac-github <88327767+tekstac-github@users.noreply.github.com>
2025-02-27 10:56:04 +0530

    Update selenium_test_booking.py

--- selenium_test_booking.py
+++ selenium_test_booking.py
@@ -10,6 +10,7 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--remote-debugging-port=9222")
+chrome_options.add_argument("--user-data-dir=/tmp/selenium_unique_dir")
 
 # Make sure the right driver is installed (like chromedriver)
 driver = webdriver.Chrome(options=chrome_options)
